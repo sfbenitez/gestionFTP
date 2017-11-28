af4219adcf5bf2b1c9937f1d3eeeed8a799b6f6b
Ferrete <sergioferretebenitez@gmail.com>
2017-11-28 23:06:29 +0100

    Adding script

---
+++ crear_pagina_web.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+DEPT=$1
+USER="user_$dept"
+
+echo "Password para consultar el servidor LDAP"
+USERUID=$(ldapsearch -x -D "uid=replica,dc=sergio,dc=gonzalonazareno,dc=org" -W uid=$USER|grep uidNumber|awk -F' ' '{ print $2 }')
+if [$USERUID == "" ];
+then
+  echo "El usuario $USER debe estar creado previamente en el servidor LDAP"
+else
+  LDAPUID=$(expr $USERUID + 1)
+  mkdir /var/www/www/$DEPT
+  chown $USERUID:apache /var/www/www/$DEPT
+  chcon -t public_content_rw_t /var/www/www/$DEPT
+fi
