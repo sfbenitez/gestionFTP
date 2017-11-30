1e6f2b80cd1ff0b4a6a617e8e2d2651050a4a865
sfbenitez <sergioferretebenitez@gmail.com>
2017-11-30 13:40:29 +0100

    adding user creation

--- crear_pagina_web.sh
+++ crear_pagina_web.sh
@@ -6,10 +6,27 @@ echo "Password para consultar el servidor LDAP"
 USERUID=$(ldapsearch -x -D "uid=replica,dc=sergio,dc=gonzalonazareno,dc=org" -W -b "dc=sergio,dc=gonzalonazareno,dc=org" uid=$USER -h mickey-int|grep uidNumber|awk -F' ' '{ print $2 }')
 if [ "$USERUID" != "" ];
 then
-  LDAPUID=$(expr $USERUID + 1)
   mkdir /var/www/www/$USER
   chown $USERUID:apache /var/www/www/$USER
   chcon -t public_content_rw_t /var/www/www/$USER
 else
-  echo "El usuario $USER debe estar creado previamente en el servidor LDAP"
+  echo "dn: uid=$USER,ou=People,dc=sergio,dc=gonzalonazareno,dc=org
+        objectClass: top
+        objectClass: posixAccount
+        objectClass: inetOrgPerson
+        objectClass: person
+        cn: $USER
+        uid: $USER
+        uidNumber: $LDAPUID
+        gidNumber: 2000
+        homeDirectory: /var/www/$DEPT
+        userPassword: {SHA}hRNsecv5/ja7nQXQY5xwwmXBjTc=
+        loginShell: /bin/bash
+        sn: $USER
+        mail: $USER@gmail.com
+        givenName: $USER" > temp.ldif
+        echo "Password de administración del servidor LDAP"
+        ldapadd x -D "cn=admin,dc=sergio,dc=gonzalonazareno,dc=org" -W -f temp.ldif
+        rm temp.ldif
+        ./crear_pagina_web.sh $DEPT
 fi
