b1b0aed1f1ed0abf2d4116148f8eb60626406c5a
Sergio Ferrete <sergioferretebenitez@gmail.com>
2017-12-17 10:38:17 +0100

    using anonymous ldap search

--- crear_pagina_web.sh
+++ crear_pagina_web.sh
@@ -2,16 +2,16 @@
 DEPT=$1
 USER="user_$DEPT"
 
-echo "Password para consultar el servidor LDAP"
-USERUID=$(ldapsearch -x -D "uid=replica,dc=sergio,dc=gonzalonazareno,dc=org" -W -b "dc=sergio,dc=gonzalonazareno,dc=org" uid=$USER -h mickey.ferrete.gonzalonazareno.org|grep uidNumber|awk -F' ' '{ print $2 }')
+echo "Busqueda anónima comprobando si existe el usuario"
+USERUID=$(ldapsearch -x -b "dc=sergio,dc=gonzalonazareno,dc=org" uid=$USER -h mickey.ferrete.gonzalonazareno.org|grep uidNumber|awk -F' ' '{ print $2 }')
 if [ "$USERUID" != "" ];
 then
   mkdir /var/www/www/$DEPT
   chown $USERUID:apache /var/www/www/$DEPT
   chcon -t public_content_rw_t /var/www/www/$DEPT
 else
-  echo "Usuario no existe, se procede a crearlo, password para consultar el último uid"
-  USERUID=$(ldapsearch -x -D "uid=replica,dc=sergio,dc=gonzalonazareno,dc=org" -W -b "dc=sergio,dc=gonzalonazareno,dc=org" uidNumber -h mickey.ferrete.gonzalonazareno.org|grep uidNumber|awk -F' ' '{ print $2 }'|tail -n1)
+  echo "Usuario no existe, se procede a crearlo, Consultando un uid que no exista"
+  USERUID=$(ldapsearch -x -b "dc=sergio,dc=gonzalonazareno,dc=org" uidNumber -h mickey.ferrete.gonzalonazareno.org|grep uidNumber|awk -F' ' '{ print $2 }'|tail -n1)
   USERUID=$(expr $USERUID + 1)
   echo "dn: uid=$USER,ou=People,dc=sergio,dc=gonzalonazareno,dc=org
 objectClass: top
